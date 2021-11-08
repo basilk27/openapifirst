8d515eea8de76cf395f6f2d780fbb0b12f0b98fe
basilk27 <basilkiwanuka@icloud.com>
2021-11-08 16:19:45 -0500

    Init

---
+++ src/main/java/com/mbsystems/openapifirst/OpenapifirstApplication.java
@@ -0,0 +1,13 @@
+package com.mbsystems.openapifirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapifirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapifirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mbsystems/openapifirst/OpenapifirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbsystems.openapifirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapifirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
